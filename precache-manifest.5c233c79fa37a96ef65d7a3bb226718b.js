52e63dafb5a7b9384de5f2a1d4a176c369263cba
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-02-23 16:28:12 +0200

    Updates

---
+++ precache-manifest.5c233c79fa37a96ef65d7a3bb226718b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07dcfa47c1c013b320cd41948f1e1b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "899ce486f5a449a5b917",
+    "url": "./static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "742231010c82232842f2",
+    "url": "./static/css/main.ca24393e.chunk.css"
+  },
+  {
+    "revision": "899ce486f5a449a5b917",
+    "url": "./static/js/2.20c71e2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.20c71e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742231010c82232842f2",
+    "url": "./static/js/main.5c2e5cfe.chunk.js"
+  },
+  {
+    "revision": "bd651092f6acbe222378",
+    "url": "./static/js/runtime-main.14f95850.js"
+  }
+]);
